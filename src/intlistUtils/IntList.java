2fdf272a551c0456ab0affd485c5eeec7d7747c7
Vladislav_Global <vladalexkirbut2003@gmail.com>
2023-03-13 19:51:13 +0300

    Created IntList interface

---
+++ src/intlistUtils/IntList.java
@@ -0,0 +1,10 @@
+package intlistUtils;
+
+public interface IntList {
+    int get(int index);
+    int set(int index, int element);
+    int size();
+    void add(int element);
+    int remove(int index);
+    int lastIndexOf(int element);
+}
